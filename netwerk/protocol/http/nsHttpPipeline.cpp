4a03456cad2442a00516319445f61ceb79511e47
Mike Perry <mikeperry-git@torproject.org>
2013-10-14 20:30:06 -0700

    Bug 5282: Randomize HTTP request order and pipeline depth.
    This is an experimental defense against Website Traffic Fingerprinting:
    http://lorre.uni.lu/~andriy/papers/acmccs-wpes11-fingerprinting.pdf
    
    See:
    https://blog.torproject.org/blog/experimental-defense-website-traffic-fingerprinting
    
    This patch is different from the approach described in that post, as well as
    the 10.x ESR patch, as the pipelining code has changed significantly between
    the time of writing of that post and Firefox 17.
    
    The main control nob for this patch is now the about:config pref
    network.http.pipelining.max-optimistic-requests. The value of that pref
    represents the minimum number of pipelined requests we will attempt to batch
    together.
    
    The total outstanding pipeline size is randomized between that value and
    network.http.pipelining.maxrequests on a per-host basis.
    
    Care must be taken when evaluating this defense, as pipeline behavior is
    extremely sensitive to browser performance. In fact, a debug build alone is
    enough to significantly impair request availability to the pipeline (due
    slower document parsing and rendering). For this reason, we provide two
    separate debug log defines. For most evaluation circumstances, you want to
    define only WTF_TEST in an optimized build to only log request order,
    combination behavior, and cases where the pipeline is forcibly disabled.
    
    This patch may also have some minor impact on SPDY request order, but the SPDY
    implementation has not been altered directly. It has several stream queues
    that may also benefit from reordering and batching, as well as a more compact
    request representation that will allow more requests to be packed inside Tor
    cells. If you have interest in evaluating SPDY in a study of Website Traffic
    Fingerprinting, please contact me.

--- netwerk/protocol/http/nsHttpPipeline.cpp
+++ netwerk/protocol/http/nsHttpPipeline.cpp
@@ -14,6 +14,11 @@
 #include "nsCOMPtr.h"
 #include "nsSocketTransportService2.h"
 #include <algorithm>
+#include "nsHttpRequestHead.h"
+
+#ifdef WTF_TEST
+#include <inttypes.h>
+#endif
 
 #ifdef DEBUG
 #include "prthread.h"
@@ -85,6 +90,32 @@ nsHttpPipeline::~nsHttpPipeline()
         free(mPushBackBuf);
 }
 
+// Generate a shuffled request ordering sequence
+void
+nsHttpPipeline::ShuffleTransOrder(uint32_t count)
+{
+   if (count < 2)
+       return;
+
+   uint32_t pos = mRequestQ[0]->PipelinePosition();
+   uint32_t i = 0;
+
+   for (i=0; i < count; ++i) {
+       uint32_t ridx = rand() % count;
+
+       nsAHttpTransaction *tmp = mRequestQ[i];
+       mRequestQ[i] = mRequestQ[ridx];
+       mRequestQ[ridx] = tmp;
+   }
+
+   for (i=0; i < count; ++i) {
+       mRequestQ[i]->SetPipelinePosition(pos);
+       pos++;
+   }
+
+   LOG(("nsHttpPipeline::ShuffleTransOrder: Shuffled %d transactions.\n", count));
+}
+
 nsresult
 nsHttpPipeline::AddTransaction(nsAHttpTransaction *trans)
 {
@@ -110,6 +141,8 @@ nsHttpPipeline::AddTransaction(nsAHttpTransaction *trans)
     // trans->SetConnection() needs to be updated to point back at
     // the pipeline object.
     trans->SetConnection(this);
+
+    ShuffleTransOrder(mRequestQ.Length());
 
     if (mConnection && !mClosed && mRequestQ.Length() == 1)
         mConnection->ResumeSend();
@@ -772,8 +805,11 @@ nsHttpPipeline::CancelPipeline(nsresult originalReason)
     if (respLen > 1)
         mResponseQ.TruncateLength(1);
 
-    DontReuse();
-    Classify(nsAHttpTransaction::CLASS_SOLO);
+    /* Don't flag timed out connections as unreusable.. Tor is just slow :( */
+    if (originalReason != NS_ERROR_NET_TIMEOUT) {
+        DontReuse();
+        Classify(nsAHttpTransaction::CLASS_SOLO);
+    }
 
     return total;
 }
@@ -853,8 +889,19 @@ nsHttpPipeline::FillSendBuf()
 
     uint32_t n;
     uint64_t avail;
+    uint64_t totalSent = 0;
+    uint64_t reqsSent = 0;
+    uint64_t alreadyPending = 0;
+
+    mSendBufIn->Available(&alreadyPending);
+
     RefPtr<nsAHttpTransaction> trans;
     nsITransport *transport = Transport();
+#ifdef WTF_TEST
+    uint64_t totalAvailable = Available();
+    RefPtr<nsHttpConnectionInfo> ci;
+    GetConnectionInfo(getter_AddRefs(ci));
+#endif
 
     while ((trans = Request(0)) != nullptr) {
         avail = trans->Available();
@@ -875,6 +922,7 @@ nsHttpPipeline::FillSendBuf()
             }
 
             mSendingToProgress += n;
+            totalSent += n;
             if (!mSuppressSendEvents && transport) {
                 // Simulate a SENDING_TO event
                 trans->OnTransportStatus(transport,
@@ -885,6 +933,20 @@ nsHttpPipeline::FillSendBuf()
 
         avail = trans->Available();
         if (avail == 0) {
+#ifdef WTF_TEST
+            nsHttpRequestHead *head = trans->RequestHead();
+			nsAutoCString requestURI;
+			if (head) {
+				head->RequestURI(requestURI);
+			} else {
+				requestURI = "<unknown?>";
+			}
+            fprintf(stderr, "WTF-order: Pipelined req %d/%d (%dB). Url: %s%s\n",
+                    trans->PipelinePosition(), PipelineDepth(), n,
+                    ci->Origin(), requestURI.get());
+#endif
+            reqsSent++;
+
             // move transaction from request queue to response queue
             mRequestQ.RemoveElementAt(0);
             mResponseQ.AppendElement(trans);
@@ -904,6 +966,15 @@ nsHttpPipeline::FillSendBuf()
         else
             mRequestIsPartial = true;
     }
+
+#ifdef WTF_TEST
+    if (totalSent)
+      fprintf(stderr,
+              "WTF-combine: Sent %" PRIu64 "/%" PRIu64 " bytes of %" PRIu64
+              " combined pipelined requests for host %s\n",
+              alreadyPending+totalSent, totalAvailable, reqsSent, ci->Origin());
+#endif
+
     return NS_OK;
 }
 
