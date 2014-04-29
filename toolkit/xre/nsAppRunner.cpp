e327874ff2696ebf4d45911e04ef2741778fdcc3
Kathy Brade <brade@pearlcrescent.com>
2014-04-29 13:08:24 -0400

    Bug #11641: change TBB directory structure to be more like Firefox's
    Unless the -osint command line flag is used, the browser now defaults
    to the equivalent of -no-remote.  There is a new -allow-remote flag that
    may be used to restore the original (Firefox-like) default behavior.

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -1559,7 +1559,8 @@ DumpHelp()
          "  --profile <path>   Start with profile at <path>.\n"
          "  --migration        Start with migration wizard.\n"
          "  --ProfileManager   Start with ProfileManager.\n"
-         "  --no-remote        Do not accept or send remote commands; implies --new-instance.\n"
+         "  --no-remote        (default) Do not accept or send remote commands; implies --new-instance.\n"
+         "  --allow-remote     Accept and send remote commands.\n"
          "  --new-instance     Open new instance, not a new window in running instance.\n"
          "  --UILocale <locale> Start with <locale> resources as UI Locale.\n"
          "  --safe-mode        Disables extensions and themes for this session.\n", gAppData->name);
@@ -3366,6 +3367,12 @@ XREMain::XRE_mainInit(bool* aExitFlag)
     gSafeMode = true;
 #endif
 
+  // In the Tor Browser, remoting is disabled by default unless -osint is used.
+  bool allowRemote = (CheckArg("allow-remote") == ARG_FOUND);
+  if (!allowRemote && (CheckArg("osint", false, nullptr, false) != ARG_FOUND)) {
+    SaveToEnv("MOZ_NO_REMOTE=1");
+  }
+
   // Handle --no-remote and --new-instance command line arguments. Setup
   // the environment to better accommodate other components and various
   // restart scenarios.
@@ -3373,8 +3380,9 @@ XREMain::XRE_mainInit(bool* aExitFlag)
   if (ar == ARG_BAD) {
     PR_fprintf(PR_STDERR, "Error: argument --no-remote is invalid when argument --osint is specified\n");
     return 1;
-  } else if (ar == ARG_FOUND) {
-    SaveToEnv("MOZ_NO_REMOTE=1");
+  } else if ((ar == ARG_FOUND) && allowRemote) {
+    PR_fprintf(PR_STDERR, "Error: argument --no-remote is invalid when argument --allow-remote is specified\n");
+    return 1;
   }
 
   ar = CheckArg("new-instance", true);
