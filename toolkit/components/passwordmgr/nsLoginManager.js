dbec05f0d8d1baabc1f811c66207d703a11bc5ce
Kathy Brade <brade@pearlcrescent.com>
2015-04-17 16:31:28 -0400

    Bug 14716: HTTP Basic Authentication prompt only displayed once
    Modify the login manager implementation to handle the situation
    where storage is not available.

--- toolkit/components/passwordmgr/nsLoginManager.js
+++ toolkit/components/passwordmgr/nsLoginManager.js
@@ -153,9 +153,25 @@ LoginManager.prototype = {
       log("No alternate nsILoginManagerStorage registered");
     }
 
-    this._storage = Cc[contractID].
-                    createInstance(Ci.nsILoginManagerStorage);
-    this.initializationPromise = this._storage.initialize();
+    // If the security.nocertdb pref. is true, we skip initialization of
+    // login manager storage since we know it will fail.  In this case we
+    // pretend that initialization succeeded in order to avoid a cascade of
+    // initialization errors.
+    if (Services.prefs.getBoolPref("security.nocertdb")) {
+      this._storage = null;
+      this.initializationPromise = Promise.resolve();
+    } else {
+      this._storage = Cc[contractID].
+                      createInstance(Ci.nsILoginManagerStorage);
+      try {
+        this.initializationPromise = this._storage.initialize();
+      } catch (e) {
+        // If storage is not available, set _storage to null so that we can
+        // cleanly check for a lack of storage elsewhere in this file.
+        this._storage = null;
+        this.initializationPromise = Promise.reject(e);
+      }
+    }
   },
 
 
@@ -196,7 +212,8 @@ LoginManager.prototype = {
         this._pwmgr = null;
       } else if (topic == "passwordmgr-storage-replace") {
         Task.spawn(function () {
-          yield this._pwmgr._storage.terminate();
+          if (this._pwmgr._storage)
+            yield this._pwmgr._storage.terminate();
           this._pwmgr._initStorage();
           yield this._pwmgr.initializationPromise;
           Services.obs.notifyObservers(null,
@@ -331,6 +348,9 @@ LoginManager.prototype = {
     if (logins.some(function(l) login.matches(l, true)))
       throw "This login already exists.";
 
+    if (!this._storage)
+      throw "No storage to add login";
+
     log("Adding login");
     return this._storage.addLogin(login);
   },
@@ -343,6 +363,12 @@ LoginManager.prototype = {
    */
   removeLogin : function (login) {
     log("Removing login");
+
+    if (!this._storage) {
+      log("No storage to remove login");
+      return null;
+    }
+
     return this._storage.removeLogin(login);
   },
 
@@ -354,6 +380,12 @@ LoginManager.prototype = {
    */
   modifyLogin : function (oldLogin, newLogin) {
     log("Modifying login");
+
+    if (!this._storage) {
+      log("No storage to modify login");
+      return null;
+    }
+
     return this._storage.modifyLogin(oldLogin, newLogin);
   },
 
@@ -369,6 +401,12 @@ LoginManager.prototype = {
    */
   getAllLogins : function (count) {
     log("Getting a list of all logins");
+
+    if (!this._storage) {
+      log("No storage to get all logins");
+      return null;
+    }
+
     return this._storage.getAllLogins(count);
   },
 
@@ -380,7 +418,10 @@ LoginManager.prototype = {
    */
   removeAllLogins : function () {
     log("Removing all logins");
-    this._storage.removeAllLogins();
+    if (!this._storage)
+      log("No storage to remove all logins");
+    else
+      this._storage.removeAllLogins();
   },
 
   /*
@@ -395,6 +436,12 @@ LoginManager.prototype = {
    */
   getAllDisabledHosts : function (count) {
     log("Getting a list of all disabled hosts");
+
+    if (!this._storage) {
+      log("No storage to get all disabled hosts");
+      return null;
+    }
+
     return this._storage.getAllDisabledHosts(count);
   },
 
@@ -407,6 +454,11 @@ LoginManager.prototype = {
   findLogins : function (count, hostname, formSubmitURL, httpRealm) {
     log("Searching for logins matching host:", hostname,
         "formSubmitURL:", formSubmitURL, "httpRealm:", httpRealm);
+
+    if (!this._storage) {
+      log("No storage to find logins");
+      return null;
+    }
 
     return this._storage.findLogins(count, hostname, formSubmitURL,
                                     httpRealm);
@@ -424,6 +476,11 @@ LoginManager.prototype = {
   searchLogins : function(count, matchData) {
    log("Searching for logins");
 
+    if (!this._storage) {
+      log("No storage to search logins");
+      return null;
+    }
+
     return this._storage.searchLogins(count, matchData);
   },
 
@@ -438,6 +495,9 @@ LoginManager.prototype = {
     log("Counting logins matching host:", hostname,
         "formSubmitURL:", formSubmitURL, "httpRealm:", httpRealm);
 
+    if (!this._storage)
+      return 0;
+
     return this._storage.countLogins(hostname, formSubmitURL, httpRealm);
   },
 
@@ -446,6 +506,9 @@ LoginManager.prototype = {
    * uiBusy
    */
   get uiBusy() {
+    if (!this._storage)
+      return false;
+
     return this._storage.uiBusy;
   },
 
@@ -454,6 +517,9 @@ LoginManager.prototype = {
    * isLoggedIn
    */
   get isLoggedIn() {
+    if (!this._storage)
+      return false;
+
     return this._storage.isLoggedIn;
   },
 
@@ -465,7 +531,7 @@ LoginManager.prototype = {
    */
   getLoginSavingEnabled : function (host) {
     log("Checking if logins to", host, "can be saved.");
-    if (!this._remember)
+    if (!this._remember || !this._storage)
       return false;
 
     return this._storage.getLoginSavingEnabled(host);
@@ -481,6 +547,9 @@ LoginManager.prototype = {
     // Nulls won't round-trip with getAllDisabledHosts().
     if (hostname.indexOf("\0") != -1)
       throw "Invalid hostname";
+
+    if (!this._storage)
+      throw "No storage to set login saving enabled";
 
     log("Login saving for", hostname, "now enabled?", enabled);
     return this._storage.setLoginSavingEnabled(hostname, enabled);
