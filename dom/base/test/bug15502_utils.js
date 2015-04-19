0227a910ea436fac9e09a773617464fe264c88ff
Arthur Edelstein <arthuredelstein@gmail.com>
2015-04-18 17:37:21 -0700

    Bug #15502, Part 2: Regression tests for blob URL isolation

---
+++ dom/base/test/bug15502_utils.js
@@ -0,0 +1,104 @@
+// Import Task.jsm
+let { Task } = SpecialPowers.Cu.import("resource://gre/modules/Task.jsm");
+
+// __listen(target, eventType, timeoutMs, useCapture)__.
+// Calls addEventListener on target, with the given eventType.
+// Returns a Promise that resolves to an Event object, if the event fires.
+// If a timeout occurs, then Promise is rejected with a "Timed out" error.
+// For use with Task.jsm.
+let listen = function (target, eventType, timeoutMs, useCapture) {
+  return new Promise(function (resolve, reject) {
+    let listenFunction = function (event) {
+      target.removeEventListener(eventType, listenFunction, useCapture);
+      resolve(event);
+    };
+    target.addEventListener(eventType, listenFunction, useCapture);
+    setTimeout(() => reject(new Error("Timed out")), timeoutMs);
+  });
+};
+
+// __receiveMessage(source)__.
+// Returns an event object for the next message received from source.
+// A Task.jsm coroutine.
+let receiveMessage = function* (source) {
+  let event;
+  do {
+    event = yield listen(self, "message", 5000, false);
+  } while (event.source !== source);
+  return event.data;
+};
+
+// __sendMessage(destination, message)__.
+// Sends a message to destination.
+let sendMessage = function (destination, message) {
+  destination.postMessage(message, "*");
+};
+
+// __appendLine(id, lineString)__.
+// Add a line of text to the innerHTML of element with id.
+let appendLine = function (id, lineString) {
+  document.getElementById(id).innerHTML += lineString + "\n";
+};
+
+// __xhr(method, url, responseType__.
+// A simple async XMLHttpRequest call.
+// Returns a promise with the response.
+let xhr = function (method, url, responseType) {
+  return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+    xhr.responseType = responseType;
+    xhr.send();
+  });
+};
+
+// __blobURLtoBlob(blobURL)__.
+// Asynchronously retrieves a blob object
+// from a blob URL. Returns a promise.
+let blobURLtoBlob = function (blobURL) {
+  return xhr("GET", blobURL, "blob");
+};
+
+// __blobToString(blob)__.
+// Asynchronously reads the contents
+// of a blob object into a string. Returns a promise.
+let blobToString = function (blob) {
+  return new Promise(function (resolve, reject) {
+    let fileReader = new FileReader();
+    fileReader.onload = function () {
+      resolve(fileReader.result);
+    };
+    fileReader.readAsText(blob);
+  });
+};
+
+// __blobURLtoString(blobURL)__.
+// Asynchronous coroutine that takes a blobURL
+// and returns the contents in a string.
+let blobURLtoString = function* (blobURL) {
+  let blob = yield blobURLtoBlob(blobURL);
+  return yield blobToString(blob);
+};
+
+// __stringToBlobURL(s)__.
+// Converts string s into a blob, and returns
+// a blob URL.
+let stringToBlobURL = function (s) {
+  let blob = new Blob([s]);
+  return URL.createObjectURL(blob);
+};
+
+// __workerIO(scriptFile, inputString)__.
+// Sends inputString for the worker, and waits
+// for the worker to return an outputString.
+// Task.jsm coroutine.
+let workerIO = function* (scriptFile, inputString) {
+  let worker = new Worker(scriptFile);
+  worker.postMessage(inputString);
+  let result = yield listen(worker, "message", 5000, false);
+  worker.terminate();
+  return result.data;
+};
