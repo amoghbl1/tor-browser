d2bfc1161084f5807f5ba9e2ecf8761af38a252e
Amogh Pradeep <amoghbl1@gmail.com>
2015-06-16 18:13:24 -0400

    Orfox: Rebranding to Orfox Filter brand.properties from preprocessor to prevent it from throwing errors

---
+++ mobile/android/branding/orfox/configure.sh
@@ -0,0 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+ANDROID_PACKAGE_NAME=info.guardianproject.orfox
+MOZ_APP_DISPLAYNAME="Orfox"
+MOZ_UPDATER=
+MOZ_ANDROID_ANR_REPORTER=
