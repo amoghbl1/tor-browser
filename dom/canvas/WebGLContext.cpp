545a8b469a62a618111c3f7fcc71bee5e2a0beb1
Mike Perry <mikeperry-git@torproject.org>
2015-06-19 20:20:00 -0700

    Bug #16005: Relax minimal mode.
    Use min values from a 2013 Moto E and a 2009 laptop with integrated graphics.

--- dom/canvas/WebGLContext.cpp
+++ dom/canvas/WebGLContext.cpp
@@ -1243,7 +1243,10 @@ WebGLContext::GetContextAttributes(Nullable<dom::WebGLContextAttributes>& retval
     result.mAlpha.Construct(mOptions.alpha);
     result.mDepth = mOptions.depth;
     result.mStencil = mOptions.stencil;
-    result.mAntialias = mOptions.antialias;
+    if (MinCapabilityMode())
+        result.mAntialias = false;
+    else
+        result.mAntialias = mOptions.antialias;
     result.mPremultipliedAlpha = mOptions.premultipliedAlpha;
     result.mPreserveDrawingBuffer = mOptions.preserveDrawingBuffer;
 }
