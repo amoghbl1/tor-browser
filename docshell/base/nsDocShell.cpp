40272439e506ecb869a57c08137cfcbc9b9abd4d
Arthur Edelstein <arthuredelstein@gmail.com>
2015-09-15 12:40:18 -0700

    Bug #16855: Allow blobs to be downloaded on first-party pages
    This patch may be made obsolete by Tanvi's work:
    https://bugzilla.mozilla.org/show_bug.cgi?id=1198559#c15

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -10482,9 +10482,14 @@ nsDocShell::DoURILoad(nsIURI* aURI,
 
   nsCOMPtr<nsINode> requestingNode;
   if (mScriptGlobal) {
-    requestingNode = mScriptGlobal->GetFrameElementInternal();
-    if (!requestingNode) {
+    if (!aFileName.IsVoid()) {
+      // File is being downloaded. Assign current document to requesting node.
       requestingNode = mScriptGlobal->GetExtantDoc();
+    } else {
+      requestingNode = mScriptGlobal->GetFrameElementInternal();
+      if (!requestingNode) {
+        requestingNode = mScriptGlobal->GetExtantDoc();
+      }
     }
   }
 
