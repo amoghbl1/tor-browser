c90567a9427eb761ac33dfbba5a25a11be8bdbf9
Arthur Edelstein <arthuredelstein@gmail.com>
2016-04-22 14:39:14 -0700

    Bug 16998: Disable link rel=preconnect

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -9706,6 +9706,10 @@ nsDocument::MaybePreLoadImage(nsIURI* uri, const nsAString &aCrossOriginAttr,
 void
 nsDocument::MaybePreconnect(nsIURI* aOrigURI, mozilla::CORSMode aCORSMode)
 {
+  // Tor Browser: Here we disable <link rel="preconnect">
+  // See #16998.
+  // TODO: Isolate preconnect by first party.
+/*
   nsCOMPtr<nsIURI> uri;
   if (NS_FAILED(aOrigURI->Clone(getter_AddRefs(uri)))) {
       return;
@@ -9740,6 +9744,7 @@ nsDocument::MaybePreconnect(nsIURI* aOrigURI, mozilla::CORSMode aCORSMode)
   } else {
     speculator->SpeculativeConnect(uri, nullptr);
   }
+*/
 }
 
 void
